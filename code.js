99b22f89e400e4696b19a0f699280382220b0334
lilybrongo <144718640+lilybrongo@users.noreply.github.com>
2024-10-25 16:05:14 +0000

    initial commit

--- code.js
+++ code.js
@@ -1,3 +1,38 @@
-function permutationSort(a) {
-    return 0;
+let found = false;//set to find if a sorted permutation has been found
+
+function permutationSort(array, temp = [], count = 0, totalLength = array.length) {
+    //base case for empty array
+    if (array.length < 1) {
+        return count;
+    }
+    //this will loop through each element in the o
+    for (let i = 0; i < array.length; i++) {
+
+        temp.push(array[i]);
+        count = permutationSort(array.slice(0, i).concat(array.slice(i + 1)), temp, count, totalLength);
+
+        if (temp.length == totalLength) {
+            count++;
+            if (isSortedArray(temp)) {
+                found = true;
+                return count;
+            }
+        }
+
+        temp.pop();
+
+    }    
+
+    return count;
+
+}    
+
+
+function isSortedArray(array) {
+    for (let i = 1; i < array.length; i++) {
+        if (array[i - 1] > array[i]) {
+            return false;
+        }
+    }
+    return true;
 }
